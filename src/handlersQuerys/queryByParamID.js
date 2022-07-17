dbc51d678e09813341880a2c50ca8b85508d02fb
rommer <rommer@tuemprende.com>
2022-07-17 00:04:09 -0500

    se crea handle para consultar una tabla por id

--- src/handlersQuerys/queryByParamID.js
+++ src/handlersQuerys/queryByParamID.js
@@ -1,9 +1,9 @@
 const connection = require("../db/connection")
 
 
-const queryByParamID = (id, table) => {
+const queryByParamID = (id, table, nameParamShear = 'id') => {
   return new Promise((resolve, _) => {
-    const sql = `SELECT * FROM ${table} WHERE id = ${id}`
+    const sql = `SELECT * FROM ${table} WHERE ${nameParamShear} = ${id}`
     connection.query(sql, (err, result) => {
       if (err) return resolve("registrado, pero ubo un error al retonar el registro")
       return resolve(result)
