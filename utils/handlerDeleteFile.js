a1cbf89bf2aed986f216bc50e05ebd280ee15d13
rommer <rommer@tuemprende.com>
2022-08-29 00:37:49 -0500

    se mejora validación del campo pathFile

--- utils/handlerDeleteFile.js
+++ utils/handlerDeleteFile.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports.handlerDeleteFile = (pathFile) => {
-  if (!pathFile) return
+  if (!pathFile || typeof pathFile !== 'string') return
 
   const cd = path.join(__dirname, '../')
   if (fs.existsSync(`${cd}${pathFile}`)) {
